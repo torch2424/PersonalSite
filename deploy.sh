51eaa019e57d4b3a5260ca546ac5a447b4937335
Aaron Turner <aaron@aaronthedev.com>
2016-09-18 11:17:21 +0000

    Finished deployment script and travis currently on branch installTravis for testing, but should be working

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,26 @@
 #Inform of deployment
 echo "Deploying aaronthedev.com..."
 
-# Unzip the included package
+# Unzip the included package into the package folder
 cd ~
-tar -xzf package.tgz
+echo "Printing directory and listing contents"
+pwd
+ls
+echo "Untaring package"
+rm -rf package
+tar -xzf package.tgz -C package
 rm package.tgz
+echo "Lisiting Directory contents one more time..."
+ls
 
 # Copy any file we want to keep from build to build
-cp www/app/config/parameters.yml build/app/config/parameters.yml
+# Such as keys, etc...
+# cp www/app/config/parameters.yml build/app/config/parameters.yml
 
 # Swap it all around, keeping the previous version aside in case something goes wrong
+echo "Backing up and deploying!"
 rm -rf /var/www/aaronthedev.com/dist-old
 mv /var/www/aaronthedev.com/dist /var/www/aaronthedev.com/dist-old
-mv build/dist /var/www/aaronthedev.com/dist
+rm -rf /var/www/aaronthedev.com/dist
+mv package/build/dist /var/www/aaronthedev.com/dist
+echo "Finished Deploying, goodbye!"
