972ba29fc678ed616a201294d4091106d242cbf1
Aaron Turner <aaron@aaronthedev.com>
2016-09-19 19:39:45 +0000

    Cleaned Up Styles, Removed some <br> tags, Updated gruntfile
    As the title states, most <br> tags were acceptable however, sinc they were in <p> tags

--- Gruntfile.js
+++ Gruntfile.js
@@ -280,32 +280,6 @@ module.exports = function(grunt) {
 			}
 		},
 
-		// The following *-min tasks will produce minified files in the dist folder
-		// By default, your `index.html`'s <!-- Usemin block --> will take care of
-		// minification. These next options are pre-configured if you do not wish
-		// to use the Usemin blocks.
-		// cssmin: {
-		//   dist: {
-		//     files: {
-		//       '<%= yeoman.dist %>/styles/main.css': [
-		//         '.tmp/styles/{,*/}*.css'
-		//       ]
-		//     }
-		//   }
-		// },
-		// uglify: {
-		//   dist: {
-		//     files: {
-		//       '<%= yeoman.dist %>/scripts/scripts.js': [
-		//         '<%= yeoman.dist %>/scripts/scripts.js'
-		//       ]
-		//     }
-		//   }
-		// },
-		// concat: {
-		//   dist: {}
-		// },
-
 		imagemin: {
 			dist: {
 				files: [{
@@ -340,7 +314,7 @@ module.exports = function(grunt) {
 				files: [{
 					expand: true,
 					cwd: '<%= yeoman.dist %>',
-					src: ['*.html', 'views/{,*/}*.html'],
+					src: ['*.html', 'views/**/*.html'],
 					dest: '<%= yeoman.dist %>'
 				}]
 			}
@@ -378,8 +352,8 @@ module.exports = function(grunt) {
 						'*.{ico,png,txt}',
 						'.htaccess',
 						'*.html',
-						'views/{,*/}*.html',
-						'images/{,*/}*.{webp}',
+						'views/**/*.html',
+						'images/**/*.{webp}',
 						'styles/fonts/{,*/}*.*'
 					]
 				}, {
@@ -423,7 +397,7 @@ module.exports = function(grunt) {
 				configFile: 'test/karma.conf.js',
 				singleRun: true
 			}
-		}
+		},
 	});
 
 
