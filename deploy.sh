85dfefb8d02e1ae614725bdab2c9d74277fecf6a
Aaron Turner <aaron@aaronthedev.com>
2016-09-19 06:53:53 +0000

    Bug fixing deployment script and travil tml

--- deploy.sh
+++ deploy.sh
@@ -10,10 +10,17 @@ pwd
 ls
 echo "Untaring package"
 rm -rf package
-tar -xzf package.tgz -C package
-rm package.tgz
-echo "Lisiting Directory contents one more time..."
+mkdir package
+echo "Lisiting Directory contents before untar into folder package..."
 ls
+tar -xzf package.tgz -C package
+echo "Lisiting Directory contents after untar into folder package..."
+ls
+rm package.tgz
+echo "Lisiting Directory contents, after complete untar and clean up, of project and tar results..."
+ls
+ls package
+ls package/projectBuild
 
 # Copy any file we want to keep from build to build
 # Such as keys, etc...
@@ -22,7 +29,9 @@ ls
 # Swap it all around, keeping the previous version aside in case something goes wrong
 echo "Backing up and deploying!"
 rm -rf /var/www/aaronthedev.com/dist-old
+mkdir /var/www/aaronthedev.com/dist-old
 mv /var/www/aaronthedev.com/dist /var/www/aaronthedev.com/dist-old
 rm -rf /var/www/aaronthedev.com/dist
-mv package/build/dist /var/www/aaronthedev.com/dist
+mkdir /var/www/aaronthedev.com/dist
+mv package/projectBuild/dist /var/www/aaronthedev.com/dist
 echo "Finished Deploying, goodbye!"
